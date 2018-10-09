02cfa6e5f801c390a18d3946709776280d4609c9
njcohen2 <njcohen2@illinois.edu>
2018-10-09 17:29:53 -0500

    Add documentation and master function structure. Happy coding!

---
+++ src/math.py
@@ -0,0 +1,9 @@
+'''Import statements: DO NOT TOUCH'''
+import scipy
+import matplotlib
+import pyglet
+import pandas as pd
+import plotly
+
+'''This is the master function that will be called at the end of the project.'''
+def create_line(file_name):
---
+++ src/parsing.py
@@ -0,0 +1,9 @@
+'''Import statements: DO NOT TOUCH'''
+import scipy
+import matplotlib
+import pyglet
+import pandas as pd
+import plotly
+
+'''This is the master function that will be called at the end of the project.'''
+def parse_file(file_name) :
