31cab8ec489e22b7fb796f5fb8d2f9e0a8a2bfaa
njcohen2 <njcohen2@illinois.edu>
2018-10-14 15:10:11 -0500

    Implement filter of Sunset neighborhood plots.

--- src/parsing.py
+++ src/parsing.py
@@ -4,11 +4,15 @@ import matplotlib
 import pyglet
 import pandas as pd
 import plotly
+import re
 
 '''This is the master function that will be called at the end of the project.'''
 def parse_file(file_path, desired_columns):
-    data = pd.read_csv(filepath_or_buffer=file_path, usecols=desired_columns, low_memory=True)
-    print(data)
+    sf_data = pd.read_csv(filepath_or_buffer=file_path, usecols=desired_columns, low_memory=True)
+    sunset_regex = re.compile(".*Sunset.*")
+    neighborhood = sf_data["Analysis Neighborhood"]
+    sf_data = sf_data[neighborhood.str.match(sunset_regex)]
+    print(sf_data)
 
 '''Think of this as your main() method when running the file'''
 def main():
