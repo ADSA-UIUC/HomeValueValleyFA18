2997e5a7d46ab84df6be67cd15748cda5ac2c4b0
joshuakim0724 <joshuakim0724@yahoo.com>
2018-10-14 15:34:45 -0500

    Test Commit

--- src/parsing.py
+++ src/parsing.py
@@ -11,11 +11,14 @@ def parse_file(file_path, desired_columns):
     sf_data = pd.read_csv(filepath_or_buffer=file_path, usecols=desired_columns, low_memory=True)
     sunset_regex = re.compile(".*Sunset.*")
     neighborhood = sf_data["Analysis Neighborhood"]
+
     sf_data = sf_data[neighborhood.str.match(sunset_regex)]
     print(sf_data)
 
 '''Think of this as your main() method when running the file'''
 def parsing_main():
+
+    # Make sure CSV file is in the correct file_path
     file_path = "..\OriginalCSV\Assessor_Historical_Secured_Property_Tax_Rolls.csv"
     desired_columns = ["Parcel Number", "Analysis Neighborhood"]
     parse_file(file_path, desired_columns)
