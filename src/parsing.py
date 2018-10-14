cff3eea7dfee6adb0d5a3dca58ad69a124e7b8a1
njcohen2 <njcohen2@illinois.edu>
2018-10-14 14:47:16 -0500

    Implement beginning of parsing and main method

--- src/parsing.py
+++ src/parsing.py
@@ -6,5 +6,15 @@ import pandas as pd
 import plotly
 
 '''This is the master function that will be called at the end of the project.'''
-def parse_file(file_path, desired_columns) :
-    return 0
+def parse_file(file_path, desired_columns):
+    data = pd.read_csv(filepath_or_buffer=file_path, usecols=desired_columns, low_memory=True)
+    print(data)
+
+'''Think of this as your main() method when running the file'''
+def main():
+    file_path = "..\OriginalCSV\Assessor_Historical_Secured_Property_Tax_Rolls.csv"
+    desired_columns = ["Parcel Number", "Analysis Neighborhood"]
+    parse_file(file_path, desired_columns)
+
+'''The code that's actually run'''
+main()
