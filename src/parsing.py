a9e47a358efdd604c3241040e527f68e3aa0fa23
njcohen2 <njcohen2@illinois.edu>
2018-10-21 14:15:49 -0500

    Rename files, restructure paths, redirectfiles.

--- src/parsing.py
+++ src/parsing.py
@@ -9,8 +9,6 @@ import re
 from src.math import *
 
 '''This is the master function that will be called at the end of the project.'''
-
-
 def parse_file(file_path, desired_columns):
     sf_data = pd.read_csv(filepath_or_buffer=file_path, usecols=desired_columns, low_memory=True)
     sunset_regex = re.compile(".*Sunset.*")
@@ -28,7 +26,7 @@ def parsing_main():
 
     # Make sure CSV file is in the correct file_path
     # NOTE IF MAC CHANGE '\' TO '/' (MAC = '/') (OTHER = '/')
-    file_path = "..\OriginalCSV\Assessor_Historical_Secured_Property_Tax_Rolls.csv"
+    file_path = "..\data\\rawdata\Assessor_Historical_Secured_Property_Tax_Rolls.csv"
 
     desired_columns = ["Use Code", "Number of Units", "Assessed Improvement Value",
                        "Analysis Neighborhood", "the_geom", "Use Definition"]
