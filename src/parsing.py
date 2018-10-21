fea7310b80acf6dddc00be9312b31b158042e5f0
njcohen2 <njcohen2@illinois.edu>
2018-10-21 14:05:48 -0500

    Add TODO for reimplementaiton of math files; move calculations to math

--- src/parsing.py
+++ src/parsing.py
@@ -5,6 +5,8 @@ import pyglet
 import pandas as pd
 import plotly
 import re
+# This lets us use all of math's defined functions
+from src.math import *
 
 '''This is the master function that will be called at the end of the project.'''
 
@@ -20,10 +22,6 @@ def parse_file(file_path, desired_columns):
     sf_data = sf_data[neighborhood.str.match(sunset_regex, na=False)]
     sf_data = sf_data[use.str.match(use_regex, na=False)]
     return sf_data
-    # print(sf_data)
-
-def calculate(original_value_filtered, intermediate_columns):
-    #calculate the area of the house
 
 '''Think of this as your main() method when running the file'''
 def parsing_main():
@@ -37,8 +35,12 @@ def parsing_main():
     original_value_filtered = parse_file(file_path, desired_columns)
     intermediate_columns = ["Use Code", "Number of Units", "Assessed Improvement Value",
                       "the_geom"]
-    output_columns = calculate(original_value_filtered, intermediate_columns)
-    print(output_columns)
+
+    #TODO: Implement the line below in math.py
+    # output_columns = calculate(original_value_filtered, intermediate_columns)
+    # print(output_columns)
+
+    print(original_value_filtered)
 
 '''The code that's actually run'''
 parsing_main()
