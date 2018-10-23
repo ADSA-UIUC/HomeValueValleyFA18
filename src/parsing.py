107813d15ad1bbdcba29ec0b59489d0e47bca04f
joshuacheng <joshuac6@illinois.edu>
2018-10-22 19:25:31 -0500

    CSV writer updated

--- src/parsing.py
+++ src/parsing.py
@@ -10,7 +10,7 @@ from src.math import *
 
 '''This is the master function that will be called at the end of the project.'''
 def parse_file(file_path, desired_columns):
-    sf_data = pd.read_csv(filepath_or_buffer=file_path, usecols=desired_columns, low_memory=True)
+    sf_data = pd.read_csv(filepath_or_buffer=file_path, usecols=desired_columns, low_memory=True, index_col=False)
     sunset_regex = re.compile(".*Sunset.*")
     neighborhood = sf_data["Analysis Neighborhood"]
     use_regex = re.compile(".*Residential.*")
@@ -21,18 +21,24 @@ def parse_file(file_path, desired_columns):
     sf_data = sf_data[use.str.match(use_regex, na=False)]
     return sf_data
 
+def write_file(file_path, processed_info):
+    processed_info.to_csv(file_path, index=False)
+
+
 '''Think of this as your main() method when running the file'''
 def parsing_main():
 
     # Make sure CSV file is in the correct file_path
     # NOTE IF MAC CHANGE '\' TO '/' (MAC = '/') (OTHER = '/')
-    file_path = "..\data\\rawdata\Assessor_Historical_Secured_Property_Tax_Rolls.csv"
+    file_path = "..\OriginalCSV\Assessor_Historical_Secured_Property_Tax_Rolls.csv"
 
     desired_columns = ["Use Code", "Number of Units", "Assessed Improvement Value",
                        "Analysis Neighborhood", "the_geom", "Use Definition"]
     original_value_filtered = parse_file(file_path, desired_columns)
     intermediate_columns = ["Use Code", "Number of Units", "Assessed Improvement Value",
                       "the_geom"]
+
+    write_file("..\data\processeddata\San_Francisco_Assessments.csv", original_value_filtered)
 
     #TODO: Implement the line below in math.py
     # output_columns = calculate(original_value_filtered, intermediate_columns)
