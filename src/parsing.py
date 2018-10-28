c8ac3b14436ed118fa979385fc68979b4bc1f2bb
njcohen2 <njcohen2@illinois.edu>
2018-10-28 14:16:01 -0500

    Refractor variables, add comments.

--- src/parsing.py
+++ src/parsing.py
@@ -9,17 +9,19 @@ import re
 from src.math import *
 
 '''This is the master function that will be called at the end of the project.'''
-def parse_file(file_path, desired_columns):
-    sf_data = pd.read_csv(filepath_or_buffer=file_path, usecols=desired_columns, low_memory=True, index_col=False)
+def parse_file(file_path, raw_columns, processed_columns):
+    raw_sf_data = pd.read_csv(filepath_or_buffer=file_path, usecols=raw_columns, low_memory=True, index_col=False)
     sunset_regex = re.compile(".*Sunset.*")
-    neighborhood = sf_data["Analysis Neighborhood"]
+    neighborhood = raw_sf_data["Analysis Neighborhood"]
     use_regex = re.compile(".*Residential.*")
-    use = sf_data["Use Definition"]
+    use = raw_sf_data["Use Definition"]
 
     # The na=false flag will skip over any entry that is none
-    sf_data = sf_data[neighborhood.str.match(sunset_regex, na=False)]
-    sf_data = sf_data[use.str.match(use_regex, na=False)]
-    return sf_data
+    raw_sf_data = raw_sf_data[neighborhood.str.match(sunset_regex, na=False)]
+    raw_sf_data = raw_sf_data[use.str.match(use_regex, na=False)]
+
+    #TODO: Return a processed form of the data
+    return raw_sf_data
 
 def write_file(file_path, processed_info):
     processed_info.to_csv(file_path, index=False)
@@ -32,19 +34,16 @@ def parsing_main():
     # NOTE IF MAC CHANGE '\' TO '/' (MAC = '/') (OTHER = '/')
     file_path = "..\data\processeddata\San_Francisco_Assessments.csv"
 
-    desired_columns = ["Use Code", "Number of Units", "Assessed Improvement Value",
+    raw_data_columns = ["Use Code", "Number of Units", "Assessed Improvement Value",
                        "Analysis Neighborhood", "the_geom", "Use Definition"]
-    original_value_filtered = parse_file(file_path, desired_columns)
-    # Why have a copy of the exact information when you can use the original as an arg?
-    # intermediate_columns = ["Use Code", "Number of Units", "Assessed Improvement Value", "the_geom"]
 
-    write_file(file_path, original_value_filtered)
+    processed_data_columns = ["Lot Number", "Center", "Radius", "Total Assessed Value",
+                         "Assessed Value Per Unit", "On Line", "Line Number"]
 
-    #TODO: Implement the line below in math.py
-    # output_columns = calculate(original_value_filtered, intermediate_columns)
-    # print(output_columns)
+    parsed_data = parse_file(file_path, raw_data_columns, processed_data_columns)
 
-    print(original_value_filtered)
+    write_file(file_path, parsed_data)
+    print(parsed_data)
 
 '''The code that's actually run'''
 parsing_main()
