c224c60629ceb27a43f058018f993a40d26b33ae
njcohen2 <njcohen2@illinois.edu>
2018-11-03 21:58:52 -0500

    Further experiment with visualization.

--- src/visualization.py
+++ src/visualization.py
@@ -4,25 +4,28 @@ import matplotlib
 import pyglet
 import pandas as pd
 import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 ''' This is the master function that will be called at the end of the project. '''
 def visualize(city_name):
     # Retrieving the information of the city itself for Layout()
     city_info_fp = "../data/processeddata/City_Info.csv"
     city_info = pd.read_csv(city_info_fp)
-    city_info.set_index("Name")
+
+    #city_info.set_index("Name")
 
     print(city_info)
 
-    city_lat = city_info[city_name]["Center-Lat"]
-    city_long = city_info[city_name]["Center-Long"]
+    city_lat = city_info.iloc[0]["Center-Lat"]
+    city_long = city_info.iloc[0]["Center-Long"]
 
     # This grabs the property data for the city
-    data_filepath = city_info[city_name]["Filename"]
+    data_filepath = "../data/processeddata/" + city_info.iloc[0]["Filename"]
     city_housing_data = pd.read_csv(data_filepath, low_memory=False)
 
     data = [
-        plotly.Scattermapbox(
+        go.Scattermapbox(
             lat=city_housing_data["Center-Lat"],
             lon=city_housing_data["Center-Long"],
             mode="markers",
@@ -32,7 +35,7 @@ def visualize(city_name):
         )
     ]
 
-    layout = plotly.Layout(
+    layout = go.Layout(
         autosize=True,
         hovermode="closest",
         mapbox=dict(
@@ -48,7 +51,7 @@ def visualize(city_name):
 
     fig = dict(data=data, layout=layout)
 
-    plotly.iplot(fig, filename="San_Francisco_Visualization")
+    py.iplot(fig, filename="San_Francisco_Visualization")
 
 
 ''' The main file for this file. '''
